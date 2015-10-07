ba446e2ed5238a3175f97392852799306846925a
Kev <wattebled.kev@gmail.com>
2015-10-07 11:40:00 +0200

    test

--- app/scripts/modules/js-basics.js
+++ app/scripts/modules/js-basics.js
@@ -19,14 +19,31 @@
         // Return the file extension (with no period) if it has one, otherwise false
         var extension;
         var pos = str.lastIndexOf('.');
-        var extension = str.substr(pos); // sous_chaine
-        return extension;
+        var extension = str.substr(pos+1); // sous_chaine
+        if(pos != -1)
+          return extension;
+        return false;
 
     };
 
     JS_BASICS.longestString = function(arr) {
         // arr will be an array.
         // Return the longest string in the array
+        var length_Max = -1;
+        var mot_max;
+        var length_cour;
+        for (var i = 0; i < arr.length; i++) {
+          if (typeof arr[i] != "string") continue;
+          length_cour = arr[i].length;
+          if (length_cour > length_Max) {
+            length_Max = length_cour;
+            mot_max = arr[i];
+          }
+        }
+
+        if(length_Max != -1)
+          return mot_max;
+        return "lol";
     };
 
     JS_BASICS.reverseString = function(str) {
